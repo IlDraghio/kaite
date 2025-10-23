a444b0f9c035cccab369990184759e57cc341e82
IlDraghio <altierifiego@gmail.com>
2025-10-23 12:52:27 +0200

    fixed redirect and csrf token for registration page - page done for now

--- kaite/user/static/user/js/registration.js
+++ kaite/user/static/user/js/registration.js
@@ -4,12 +4,13 @@ form.addEventListener("submit", async function(event) {
 
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
-
+    const csrfToken = document.querySelector('#registration-form input[name="csrfmiddlewaretoken"]').value;
+    
     const response = await fetch(registrationUrl, {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
-            "X-CSRFToken": "{{ csrf_token }}",
+            "X-CSRFToken": csrfToken,
         },
         body: JSON.stringify({username, password})
     });
@@ -17,8 +18,15 @@ form.addEventListener("submit", async function(event) {
     const data = await response.json();
 
     if (response.ok) {
-        showPopup(`User ${data.username} registered successfully!`);
+        try {
+        showPopup(`User ${data.username} registered successfully!\nRedirecting to the login page...`);
         form.reset();
+        setTimeout(() => {
+        window.location.href = loginUrl;
+        }, 3010);
+        } catch (err) {
+        console.error("Error inside success block:", err);
+    }
     } else {
         let errors = "";
         for (const key in data) errors += `${key}: ${data[key]}\n`;
